95dada15085ece8c8c6b2ae7ad09d64c18d41eb6
pnugues <pierre.nugues@cs.lth.se>
2014-03-02 15:50:21 +0100

    Small type

--- programs/ch02/regex13.pl
+++ programs/ch02/regex13.pl
@@ -1,7 +1,7 @@
 # This must be run with the command:
 # perl -CS prog.pl
 # Printing Unicode names, 
-use charnames ':full'; # To uses Unicode names
+use charnames ':full'; # To use Unicode names
 
 # Unicode names
 print "\N{LATIN CAPITAL LIGATURE OE}, \N{LATIN CAPITAL LETTER O WITH DIAERESIS}, \N{LATIN SMALL LETTER O WITH DIAERESIS}, \x{0152}, \x{00D6}", " \077 " , "\x40\n";
