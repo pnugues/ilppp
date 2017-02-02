2750f4728e6cbbdfbfe3c4a738616c433724745e
Pierre Nugues <pierre.nugues@cs.lth.se>
2017-02-02 16:00:55 +0100

    Analytical regression with a notebook

--- programs/ch04/python/analytical_regression.py
+++ programs/ch04/python/analytical_regression.py
@@ -10,58 +10,87 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def regression_matrix(observations, reg=0.0):
+def regression_matrix(X, y, reg=0.0):
     """
     Computes the regression using numpy matrices
     :param observations, regularization
-    :return: weights
+    :return: weights, ŷ, se, sse
     """
     if reg != 0.0:
         print('Regularized')
-    X = np.matrix(observations)[:, :-1]
-    y = np.matrix(observations)[:, -1]
     I = np.identity(X.shape[1])
-
     w = (X.T * X + reg * I).I * X.T * y
     y_hat = X * w
+    se = np.square(y_hat - y)
     sse = (y_hat - y).T * (y_hat - y)
-    print("Weights", w.T)
-    print("SSE", sse)
-    return w
+    return w, y_hat, se, sse
 
 
-def regression_array(observations):
+def regression_array(X, y):
     """
     Computes the regression using numpy arrays
     :param observations:
-    :return:
+    :return: weights, ŷ, sse
     """
-    X = np.array(observations)[:, :-1]
-    y = np.array(observations)[:, -1]
     w = np.dot(np.dot(np.linalg.inv(np.dot(X.T, X)), X.T), y)
     # Or directly with pinv()
     # w = np.dot(np.linalg.pinv(X), y)
     y_hat = np.dot(X, w)
     sse = np.dot((y_hat - y).T, (y_hat - y))
-    print("Weights", w.T)
-    print("SSE", sse)
-    return w
+    return w, y_hat, sse
 
 
 if __name__ == '__main__':
+    # Loading the data set
     stat_en = open('../salammbo/salammbo_a_en.tsv').read().strip().split('\n')
     stat_fr = open('../salammbo/salammbo_a_fr.tsv').read().strip().split('\n')
     pattern = [('red', 's'), ('green', '^')]
     lang = [None] * 2
+
+    # Regression with arrays
     for i, stats in enumerate([stat_en, stat_fr]):
         observations = [[1] + list(map(float, obs.split())) for obs in stats]
-        x = [obs[1] for obs in observations]
-        y = [obs[2] for obs in observations]
-        lang[i] = plt.scatter(x, y, color=pattern[i][0], marker=pattern[i][1])
-        # w = regression_array(observations)
-        w = np.array(regression_matrix(observations))
-        plt.plot([min(x), max(x)],
-                 [np.dot([1, min(x)], w), np.dot([1, max(x)], w)],
+        x_l = [obs[1] for obs in observations]
+        y_l = [obs[2] for obs in observations]
+        lang[i] = plt.scatter(x_l, y_l, color=pattern[i][0], marker=pattern[i][1])
+        X = np.array(observations)[:, :-1]
+        y = np.array(observations)[:, -1]
+        w, y_hat, sse = regression_array(X, y)
+        print('Language:', i)
+        print('X:', X)
+        print('y:', y)
+        print('ŷ:', y_hat)
+        print("Weights", w.T)
+        print("SSE", sse)
+        plt.plot([min(x_l), max(x_l)],
+                 [np.dot([1, min(x_l)], w), np.dot([1, max(x_l)], w)],
+                 color=pattern[i][0])
+    plt.title("Salammbô")
+    plt.xlabel("Letter count")
+    plt.ylabel("A count")
+    plt.legend((lang[0], lang[1]), ('English', 'French'), loc='lower right', scatterpoints=1)
+    plt.show()
+
+    # With matrices
+    for i, stats in enumerate([stat_en, stat_fr]):
+        observations = [[1] + list(map(float, obs.split())) for obs in stats]
+        x_l = [obs[1] for obs in observations]
+        y_l = [obs[2] for obs in observations]
+        lang[i] = plt.scatter(x_l, y_l, color=pattern[i][0], marker=pattern[i][1])
+        X = np.matrix(observations)[:, :-1]
+        y = np.matrix(observations)[:, -1]
+        w, y_hat, se, sse = regression_matrix(X, y)
+        print('Language:', i)
+        print('X:', X)
+        print('y:', y)
+        print('ŷ:', y_hat)
+        print('Squared errors:', se)
+        print("Weights", w.T)
+        print("SSE", sse)
+
+        w = np.array(w)
+        plt.plot([min(x_l), max(x_l)],
+                 [np.dot([1, min(x_l)], w), np.dot([1, max(x_l)], w)],
                  color=pattern[i][0])
     plt.title("Salammbô")
     plt.xlabel("Letter count")
@@ -72,17 +101,32 @@ if __name__ == '__main__':
     print('Trying regularization with a singular matrix')
     # Creation of a singular matrix by duplicating a column
     observations = [obs[0:-1] + [obs[-2]] + [obs[-1]] for obs in observations]
+    X = np.matrix(observations)[:, :-1]
+    y = np.matrix(observations)[:, -1]
+    print('X:', X)
+    print('y:', y)
     try:
-        regression_matrix(observations)
+        regression_matrix(X, y)
     except:
         print(np.linalg.linalg.LinAlgError)
         print("Singular matrix: Could not be inverted.")
-    regression_matrix(observations, reg=0.01)
+
+    # Singular matrix with regularization
+    w, y_hat, se, sse = regression_matrix(X, y, reg=0.01)
+    print(w, y_hat, se, sse)
 
     # Trying regularization with a quasi singular matrix
     print('Trying regularization with a quasi singular matrix')
+    np.set_printoptions(precision=10)
     observations[0][2] -= 0.000001
+    X = np.matrix(observations)[:, :-1]
+    y = np.matrix(observations)[:, -1]
+    print('X:', X)
+    print('y:', y)
     # No regularization
-    regression_matrix(observations)
+    w, y_hat, se, sse = regression_matrix(X, y)
+    print(w, y_hat, se, sse)
+
     # With regularization
-    regression_matrix(observations, reg=0.01)
+    w, y_hat, se, sse = regression_matrix(X, y, reg=0.01)
+    print(w, y_hat, se, sse)
