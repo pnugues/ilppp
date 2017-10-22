4d50d1d15bbfd1ebe8b5444293d6ab2790fc3c6c
Pierre Nugues <pierre.nugues@cs.lth.se>
2017-10-22 18:09:31 +0200

    Removal of useless class encoding

--- programs/labs/chunking/chunker_python/ml_chunker.py
+++ programs/labs/chunking/chunker_python/ml_chunker.py
@@ -11,7 +11,7 @@ from sklearn import linear_model
 from sklearn import metrics
 from sklearn import tree
 from sklearn.naive_bayes import GaussianNB
-from sklearn.grid_search import GridSearchCV
+from sklearn.model_selection import GridSearchCV
 
 
 def extract_features(sentences, w_size, feature_names):
@@ -83,58 +83,16 @@ def extract_features_sent(sentence, w_size, feature_names):
     return X, y
 
 
-def encode_classes(y_symbols):
-    """
-    Encode the classes as numbers
-    :param y_symbols:
-    :return: the y vector and the lookup dictionaries
-    """
-    # We extract the chunk names
-    classes = sorted(list(set(y_symbols)))
-    """
-    Results in:
-    ['B-ADJP', 'B-ADVP', 'B-CONJP', 'B-INTJ', 'B-LST', 'B-NP', 'B-PP',
-    'B-PRT', 'B-SBAR', 'B-UCP', 'B-VP', 'I-ADJP', 'I-ADVP', 'I-CONJP',
-    'I-INTJ', 'I-NP', 'I-PP', 'I-PRT', 'I-SBAR', 'I-UCP', 'I-VP', 'O']
-    """
-    # We assign each name a number
-    dict_classes = dict(enumerate(classes))
-    """
-    Results in:
-    {0: 'B-ADJP', 1: 'B-ADVP', 2: 'B-CONJP', 3: 'B-INTJ', 4: 'B-LST',
-    5: 'B-NP', 6: 'B-PP', 7: 'B-PRT', 8: 'B-SBAR', 9: 'B-UCP', 10: 'B-VP',
-    11: 'I-ADJP', 12: 'I-ADVP', 13: 'I-CONJP', 14: 'I-INTJ',
-    15: 'I-NP', 16: 'I-PP', 17: 'I-PRT', 18: 'I-SBAR',
-    19: 'I-UCP', 20: 'I-VP', 21: 'O'}
-    """
-
-    # We build an inverted dictionary
-    inv_dict_classes = {v: k for k, v in dict_classes.items()}
-    """
-    Results in:
-    {'B-SBAR': 8, 'I-NP': 15, 'B-PP': 6, 'I-SBAR': 18, 'I-PP': 16, 'I-ADVP': 12,
-    'I-INTJ': 14, 'I-PRT': 17, 'I-CONJP': 13, 'B-ADJP': 0, 'O': 21,
-    'B-VP': 10, 'B-PRT': 7, 'B-ADVP': 1, 'B-LST': 4, 'I-UCP': 19,
-    'I-VP': 20, 'B-NP': 5, 'I-ADJP': 11, 'B-CONJP': 2, 'B-INTJ': 3, 'B-UCP': 9}
-    """
-
-    # We convert y_symbols into a numerical vector
-    y = [inv_dict_classes[i] for i in y_symbols]
-    return y, dict_classes, inv_dict_classes
-
-
 def predict(test_sentences, feature_names, f_out):
     for test_sentence in test_sentences:
-        X_test_dict, y_test_symbols = extract_features_sent(test_sentence, w_size, feature_names)
+        X_test_dict, y_test = extract_features_sent(test_sentence, w_size, feature_names)
         # Vectorize the test sentence and one hot encoding
         X_test = vec.transform(X_test_dict)
         # Predicts the chunks and returns numbers
         y_test_predicted = classifier.predict(X_test)
-        # Converts to chunk names
-        y_test_predicted_symbols = list(dict_classes[i] for i in y_test_predicted)
         # Appends the predicted chunks as a last column and saves the rows
         rows = test_sentence.splitlines()
-        rows = [rows[i] + ' ' + y_test_predicted_symbols[i] for i in range(len(rows))]
+        rows = [rows[i] + ' ' + y_test_predicted[i] for i in range(len(rows))]
         for row in rows:
             f_out.write(row + '\n')
         f_out.write('\n')
@@ -143,8 +101,8 @@ def predict(test_sentences, feature_names, f_out):
 
 if __name__ == '__main__':
     start_time = time.clock()
-    train_corpus = '../../corpus/conll2000/train.txt'
-    test_corpus = '../../corpus/conll2000/test.txt'
+    train_corpus = '../../../corpus/conll2000/train.txt'
+    test_corpus = '../../../corpus/conll2000/test.txt'
     w_size = 2  # The size of the context window to the left and right of the word
     feature_names = ['word_n2', 'word_n1', 'word', 'word_p1', 'word_p2',
                      'pos_n2', 'pos_n1', 'pos', 'pos_p1', 'pos_p2']
@@ -152,17 +110,15 @@ if __name__ == '__main__':
     train_sentences = conll_reader.read_sentences(train_corpus)
 
     print("Extracting the features...")
-    X_dict, y_symbols = extract_features(train_sentences, w_size, feature_names)
+    X_dict, y = extract_features(train_sentences, w_size, feature_names)
 
-    print("Encoding the features and classes...")
+    print("Encoding the features...")
     # Vectorize the feature matrix and carry out a one-hot encoding
     vec = DictVectorizer(sparse=True)
     X = vec.fit_transform(X_dict)
     # The statement below will swallow a considerable memory
     # X = vec.fit_transform(X_dict).toarray()
     # print(vec.get_feature_names())
-
-    y, dict_classes, inv_dict_classes = encode_classes(y_symbols)
 
     training_start_time = time.clock()
     print("Training the model...")
@@ -177,10 +133,9 @@ if __name__ == '__main__':
     # Here we carry out a chunk tag prediction and we report the per tag error
     # This is done for the whole corpus without regard for the sentence structure
     print("Predicting the chunks in the test set...")
-    X_test_dict, y_test_symbols = extract_features(test_sentences, w_size, feature_names)
+    X_test_dict, y_test = extract_features(test_sentences, w_size, feature_names)
     # Vectorize the test set and one-hot encoding
     X_test = vec.transform(X_test_dict)  # Possible to add: .toarray()
-    y_test = [inv_dict_classes[i] if i in y_symbols else 0 for i in y_test_symbols]
     y_test_predicted = classifier.predict(X_test)
     print("Classification report for classifier %s:\n%s\n"
           % (classifier, metrics.classification_report(y_test, y_test_predicted)))
