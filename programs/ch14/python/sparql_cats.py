fc7aea7b292e1ae15929a83cd0c2016699b95923
Pierre Nugues <pierre.nugues@cs.lth.se>
2018-10-01 15:04:48 +0200

    added sparql

---
+++ programs/ch14/python/sparql_cats.py
@@ -0,0 +1,38 @@
+"""
+Extraction of cat names, replicating an example from https://query.wikidata.org/
+__author__ = "Pierre Nugues"
+"""
+import requests
+import pandas as pd
+
+pd.options.display.max_rows = 10000
+pd.options.display.max_columns = 80
+pd.options.display.width = 200
+
+query = '''PREFIX wikibase: <http://wikiba.se/ontology#>
+PREFIX wd: <http://www.wikidata.org/entity/>
+PREFIX wdt: <http://www.wikidata.org/prop/direct/>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+
+SELECT ?item ?itemLabel WHERE {
+    ?item wdt:P31 wd:Q146 .
+
+    OPTIONAL {
+        ?item rdfs:label ?itemLabel filter (lang(?itemLabel) = "en") .
+    }
+}'''
+
+url = 'https://query.wikidata.org/bigdata/namespace/wdq/sparql'
+data = requests.get(url, params={'query': query, 'format': 'json'}).json()
+
+# print(data)
+cats = []
+for item in data['results']['bindings']:
+    # print(item)
+    cats.append({
+        'id': item['item']['value'],
+        'name': item.get('itemLabel', {}).get('value')})
+
+df = pd.DataFrame(cats)
+print(len(df))
+print(df)
---
+++ programs/ch14/python/sparql_occupations.py
@@ -0,0 +1,41 @@
+"""
+Extraction of cat names, replicating an example from https://query.wikidata.org/
+__author__ = "Pierre Nugues"
+"""
+import requests
+import pandas as pd
+
+pd.options.display.max_rows = 10000
+pd.options.display.max_columns = 80
+pd.options.display.width = 200
+
+query = '''PREFIX wikibase: <http://wikiba.se/ontology#>
+PREFIX wd: <http://www.wikidata.org/entity/>
+PREFIX wdt: <http://www.wikidata.org/prop/direct/>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+
+SELECT ?item ?itemLabel ?_subclass_of ?_subclass_ofLabel
+WHERE {
+  ?item wdt:P31 wd:Q28640; # instance of Profession
+        wdt:P279 ?_subclass_of. # subclass of
+  OPTIONAL {
+    ?item rdfs:label ?itemLabel filter (lang(?itemLabel) = "en") .
+  }
+  OPTIONAL {
+    ?_subclass_of rdfs:label ?_subclass_ofLabel filter (lang(?_subclass_ofLabel) = "en") .
+  }
+}'''
+
+url = 'https://query.wikidata.org/bigdata/namespace/wdq/sparql'
+data = requests.get(url, params={'query': query, 'format': 'json'}).json()
+
+profession = []
+for item in data['results']['bindings']:
+    profession.append({
+        'id': item['item']['value'],
+        'name': item.get('itemLabel', {}).get('value'),
+        'subclass': item.get('_subclass_ofLabel', {}).get('value')})
+
+df = pd.DataFrame(profession)
+print(len(df))
+print(df)
