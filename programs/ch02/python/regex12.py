eecdf0429dddf73f26ba709ab33a5fda9455db21
Pierre Nugues <pierre.nugues@cs.lth.se>
2020-08-31 14:43:12 +0200

    updates 2020

--- programs/ch02/python/regex12.py
+++ programs/ch02/python/regex12.py
@@ -3,29 +3,37 @@ Detecting nonASCII, Greek, Greek blocks
 This must be run with the command:
 python3 regex12.py <../test.txt
 """
-__author__ = "Pierre Nugues"
+__author__ = 'Pierre Nugues'
 
 import sys
-import regex
+import regex as re
 
 for line in sys.stdin:
-    # if  regex.search(r"^\p{IsASCII}+$", line):
-    #        print("line is ASCII: ", line, end="")
-    #    if regex.search(r"\P{IsASCII}", line):
-    #        print("line contains NonASCII: ", line, end="")
-    if regex.search(r"\P{InBasic_Latin}", line):
-        print("line contains non basic Latin: ", line, end="")
-    if regex.search(r"^\p{InGreek_and_Coptic}+$", line):
-        print("Line is in Greek and Coptic block: ", line, end="")
-    if regex.search(r"^\p{Greek}+$", line):
-        print("Line is Greek script: ", line, end="")
-    if regex.search(r"\u03B1", line):
-        print("Line contains alpha (code point): ", line, end="")
-    if regex.search(r'\N{GREEK SMALL LETTER ALPHA}', line):
-        print("Line contains alpha (Unicode name): ", line, end="")
-    if regex.search("α", line):
-        print("Line contains alpha (Direct char input): ", line, end="")
-    if regex.search(r"\N{LATIN SMALL LETTER A}", line):
-        print("Line contains a: ", line, end="")
-    if regex.search(r"\N{LATIN SMALL LETTER O WITH DIAERESIS}", line):
-        print("Line contains ö: ", line, end="")
+    print("Line:", line, end='')
+    if re.search(r'[\p{IsAlpha}\p{Nd}]+', line):
+        print('\tline is Alpha num')
+    """
+    # Does not work in Python
+    if re.search(r'^\p{IsASCII}+$', line):
+        print('\tline is ASCII')
+    if re.search(r'\P{IsASCII}', line):
+        print('\tline contains NonASCII')
+    """
+    if re.search(r'^\p{InBasic_Latin}+$', line):
+        print('\tline is basic Latin')
+    if re.search(r'\P{InBasic_Latin}', line):
+        print('\tline contains non basic Latin')
+    if re.search(r'^\p{InGreek_and_Coptic}+$', line):
+        print('\tLine is in Greek and Coptic block')
+    if re.search(r'^\p{Greek}+$', line):
+        print('\tLine is Greek script')
+    if re.search(r'\u03B1', line):
+        print('\tLine contains alpha (code point)')
+    if re.search(r'\N{GREEK SMALL LETTER ALPHA}', line):
+        print('\tLine contains alpha (Unicode name)')
+    if re.search('α', line):
+        print('\tLine contains alpha (Direct char input)')
+    if re.search(r'\N{LATIN SMALL LETTER A}', line):
+        print('\tLine contains letter a')
+    if re.search(r'\N{LATIN SMALL LETTER O WITH DIAERESIS}', line):
+        print('\tLine contains letter ö')
