1c96726b35fe9121572b27f4958184ea4f858ead
Pierre Nugues <pierre.nugues@cs.lth.se>
2022-08-29 15:46:58 +0200

    regex updated

--- programs/ch03/python/unicode.py
+++ programs/ch03/python/unicode.py
@@ -7,13 +7,13 @@ __author__ = "Pierre Nugues"
 import regex as re
 
 alphabet = 'αβγδεζηθικλμνξοπρστυφχψω'
-match = re.search('^\p{InBasic_Latin}+$', alphabet)
+match = re.search(r'^\p{InBasic_Latin}+$', alphabet)
 print(match)  # None
-match = re.search('^\p{InGreek_and_Coptic}+$', alphabet)
+match = re.search(r'^\p{InGreek_and_Coptic}+$', alphabet)
 print(match)  # matches alphabet
-match = re.search('^\p{Greek}+$', alphabet)
+match = re.search(r'^\p{Greek}+$', alphabet)
 print(match)  # matches alphabet
-match = re.search('\N{GREEK SMALL LETTER ALPHA}', alphabet)
+match = re.search(r'\N{GREEK SMALL LETTER ALPHA}', alphabet)
 print(match)  # matches 'α'
 match = re.search('α', alphabet)
 print(match)  # matches 'α'
